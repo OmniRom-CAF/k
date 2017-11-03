ed3bef721862887f09b87f94ea0f91c9856eaffb
dd3boh <dade.garberi@gmail.com>
2017-11-03 19:48:41 +0100

    msm8996-common: Initial sepolicy
    * Lots of parts are from the old one so thanks to everyone who contributed to that one
    * Still it doesn't boot in enforcing though
    
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,7 +162,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
 # include device/qcom/sepolicy/sepolicy.mk
-# BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Tap to wake 
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touch/tp_dev/gesture_on"
